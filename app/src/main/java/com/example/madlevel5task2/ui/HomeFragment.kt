c21a38e08c3e882c23fea2133234dabc53f6647e
Jelle <zuiderveldjelle@gmail.com>
2020-12-06 12:31:53 +0100

    added database/rep/dao/model and adapter

--- app/src/main/java/com/example/madlevel5task2/ui/HomeFragment.kt
+++ app/src/main/java/com/example/madlevel5task2/ui/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.madlevel5task2
+package com.example.madlevel5task2.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,11 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.madlevel5task2.R
+import com.example.madlevel5task2.model.Game
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class HomeFragment : Fragment() {
+
+    private val games: ArrayList<Game> = arrayListOf()
+    private val homeAdapter: HomeAdapter = HomeAdapter(games)
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +30,9 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+    }
+
+    private fun initView(){
+        
     }
 }
--- app/src/main/java/com/example/madlevel5task2/ui/MainActivity.kt
+++ app/src/main/java/com/example/madlevel5task2/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.madlevel5task2
+package com.example.madlevel5task2.ui
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -6,18 +6,20 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.example.madlevel5task2.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        navController = findNavController(R.id.nav_host_fragment)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
